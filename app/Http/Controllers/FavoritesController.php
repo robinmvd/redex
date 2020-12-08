d8cb36336072268fa8a048b2a8ab43521edc551b
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-12-08 13:00:44 +0100

    Add favorites controller.

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Support\Facades\Auth;
+
+class FavoritesController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+
+        $favorites = $user->favorites()
+            // gebruik pivot table's created_at column om te sorteren.
+            ->orderByDesc('favorites.created_at')
+            ->paginate(10);
+
+        return view('books.favorites', ['favorites' => $favorites]);
+    }
+
+    public function update(Book $book)
+    {
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+
+        if ($user->favorites()->where('book_id', $book->id)->exists()) {
+            return redirect()->back()
+                ->with('error', 'This item is already in your favorites!');
+        }
+
+        //->favorites() geeft een BelongsToMany relatie terug
+        // hierdoor kan ik de ->attach() method gebruiken.
+        $user->favorites()->attach($book);
+
+        return redirect()->back()
+            ->with('success', 'Book, ' . $book->title . ' Added to your reading list.');
+    }
+
+    public function destroy(Book $book)
+    {
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+
+        //->favorites() geeft een BelongsToMany relatie terug
+        // hierdoor kan ik de ->detach() method gebruiken.
+        $user->favorites()->detach($book);
+
+        return redirect()->back()
+            ->with('success', 'Book removed from favorites');
+    }
+}
