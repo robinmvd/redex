c43f56d8525d3a5c439ae9906d7a5cfb547812bf
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-12-08 13:05:15 +0100

    Add favorites blade partial.

---
+++ resources/views/books/favorites.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="card mb-3">
+            <ul class="list-group list-group-flush">
+                @forelse($favorites as $book)
+                    <li class="list-group-item">
+                        <p class="card-title">{{ $book->title }}</p>
+
+                        <a href="{{ route('books.show', ['book' => $book]) }}" class="card-link">
+                            Lees meer
+                        </a>
+                    </li>
+                @empty
+                    <li class="list-group-item text-center">
+                        <em class="card-text">Je hebt nog geen favorite boeken</em>
+                    </li>
+                @endforelse
+            </ul>
+        </div>
+
+        {{ $favorites->links() }}
+    </div>
+@endsection
