1a6ce79df4d4af32c0d05d1841c18364d6fbafa8
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-12-08 13:23:13 +0100

    Add favorties route.

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,31 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BooksController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ChangeBookVisibilityController;
 use App\Http\Controllers\CommentsController;
-use \App\Http\Controllers\CategoryController;
+use App\Http\Controllers\FavoritesController;
+use App\Http\Controllers\PanelController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', [BooksController::class, 'index'])->name('books');
+Route::redirect('/', '/books')->name('home');
+
+Route::get('/books/panel', [PanelController::class, 'index'])->name('books.panel');
+
+Route::put('/books/{book}/visible', [ChangeBookVisibilityController::class, 'makeVisible'])->name('books.visible');
+Route::put('/books/{book}/hidden', [ChangeBookVisibilityController::class, 'makeHidden'])->name('books.hidden');
 
 Route::resource('books', BooksController::class);
+Route::resource('books.comments', CommentsController::class)->only(['store']);
 
+Route::resource('categories', CategoryController::class)->only(['show']);
 
-Route::get('/panel', [BooksController::class, 'panel'])->name('books.panel');
-
-Route::get('/status/update', [BooksController::class, 'updateStatus'])->name('books.update.status');
-
-Route::post('/books/{book}/comments', [CommentsController::class, 'store'])->name('comments.store');
-
-Route::get('/home', [BooksController::class, 'index'])->name('home');
-
-Route::get('/search', [BooksController::class, 'search']);
-
-Route::get('/books/categories/{category}', [CategoryController::class, 'index']);
-
-
-
+Route::resource('favorites', FavoritesController::class)
+    //gebruik 'Book' model i.p.v. 'Favorite' model als de controller.
+    //verwacht een 'Book' parameter bij de 'update en 'destroy' methodes.
+    ->parameter('favorites', 'book')
+    //gebruik alleen deze routes voor de resource controller.
+    ->only(['index', 'update', 'destroy']);
